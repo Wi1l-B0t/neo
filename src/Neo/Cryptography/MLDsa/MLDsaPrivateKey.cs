df98543bae4d05414e37151d6c09cf69308dee82
Wi1l-B0t <201105916+Wi1l-B0t@users.noreply.github.com>
2025-11-18 22:46:08 +0800

    Add: ML-DSA65 support

--- src/Neo/Cryptography/MLDsa/MLDsaPrivateKey.cs
+++ src/Neo/Cryptography/MLDsa/MLDsaPrivateKey.cs
@@ -13,61 +13,59 @@ using Org.BouncyCastle.Crypto.Generators;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Crypto.Signers;
 using Org.BouncyCastle.Security;
-using System;
 
-namespace Neo.Cryptography
+namespace Neo.Cryptography;
+
+public class MLDsaPrivateKey
 {
-    public class MLDsaPrivateKey
+    private readonly MLDsaPrivateKeyParameters _privateKey;
+
+    public MLDsaPrivateKey(MLDsaPrivateKeyParameters privateKey)
     {
-        private readonly MLDsaPrivateKeyParameters _privateKey;
+        _privateKey = privateKey;
+    }
 
-        public MLDsaPrivateKey(MLDsaPrivateKeyParameters privateKey)
-        {
-            _privateKey = privateKey;
-        }
+    public static MLDsaPrivateKey CreateMLDsa65()
+    {
+        // C# standard library does not all platforms, so we use BouncyCastle.
+        var generator = new MLDsaKeyPairGenerator();
+        generator.Init(new MLDsaKeyGenerationParameters(new SecureRandom(), MLDsaParameters.ml_dsa_65));
 
-        public static MLDsaPrivateKey CreateMLDsa65()
-        {
-            // C# standard library does not all platforms, so we use BouncyCastle.
-            var generator = new MLDsaKeyPairGenerator();
-            generator.Init(new MLDsaKeyGenerationParameters(new SecureRandom(), MLDsaParameters.ml_dsa_65));
+        var keypair = generator.GenerateKeyPair();
+        return new MLDsaPrivateKey((MLDsaPrivateKeyParameters)keypair.Private);
+    }
 
-            var keypair = generator.GenerateKeyPair();
-            return new MLDsaPrivateKey((MLDsaPrivateKeyParameters)keypair.Private);
-        }
+    public MLDsaPublicKey PublicKey => new MLDsaPublicKey(_privateKey.GetPublicKey());
 
-        public MLDsaPublicKey PublicKey => new MLDsaPublicKey(_privateKey.GetPublicKey());
+    /// <summary>
+    /// Exports the private key of the MLDsa65 with the FIPS 204 key format.
+    /// NOTE: use it carefully, because the private key is sensitive data.
+    /// </summary>
+    /// <returns>The private key in the FIPS 204 key format.</returns>
+    public byte[] ExportPrivateKey() => _privateKey.GetEncoded();
 
-        /// <summary>
-        /// Exports the private key of the MLDsa65 with the FIPS 204 key format.
-        /// NOTE: use it carefully, because the private key is sensitive data.
-        /// </summary>
-        /// <returns>The private key in the FIPS 204 key format.</returns>
-        public byte[] ExportPrivateKey() => _privateKey.GetEncoded();
+    /// <summary>
+    /// Imports the private key of the MLDsa65 with the FIPS 204 key format.
+    /// </summary>
+    /// <param name="privateKey">The private key in the FIPS 204 key format.</param>
+    /// <returns>The MLDsa65 private key imported from the private key.</returns>
+    /// <exception cref="ArgumentException">The private key is invalid.</exception>
+    public static MLDsaPrivateKey ImportMLDsa65Key(byte[] privateKey)
+    {
+        var key = MLDsaPrivateKeyParameters.FromEncoding(MLDsaParameters.ml_dsa_65, privateKey);
+        return new MLDsaPrivateKey(key);
+    }
 
-        /// <summary>
-        /// Imports the private key of the MLDsa65 with the FIPS 204 key format.
-        /// </summary>
-        /// <param name="privateKey">The private key in the FIPS 204 key format.</param>
-        /// <returns>The MLDsa65 private key imported from the private key.</returns>
-        /// <exception cref="ArgumentException">The private key is invalid.</exception>
-        public static MLDsaPrivateKey ImportMLDsa65Key(byte[] privateKey)
-        {
-            var key = MLDsaPrivateKeyParameters.FromEncoding(MLDsaParameters.ml_dsa_65, privateKey);
-            return new MLDsaPrivateKey(key);
-        }
-
-        /// <summary>
-        /// Signs the specified message with the MLDsa65.
-        /// </summary>
-        /// <param name="message">The message to sign.</param>
-        /// <returns>The signature of the message.</returns>
-        public byte[] Sign(ReadOnlySpan<byte> message)
-        {
-            var signer = new MLDsaSigner(_privateKey.Parameters, deterministic: true);
-            signer.Init(forSigning: true, _privateKey);
-            signer.BlockUpdate(message);
-            return signer.GenerateSignature();
-        }
+    /// <summary>
+    /// Signs the specified message with the MLDsa65.
+    /// </summary>
+    /// <param name="message">The message to sign.</param>
+    /// <returns>The signature of the message.</returns>
+    public byte[] Sign(ReadOnlySpan<byte> message)
+    {
+        var signer = new MLDsaSigner(_privateKey.Parameters, deterministic: true);
+        signer.Init(forSigning: true, _privateKey);
+        signer.BlockUpdate(message);
+        return signer.GenerateSignature();
     }
 }
--- src/Neo/Cryptography/MLDsa/MLDsaPublicKey.cs
+++ src/Neo/Cryptography/MLDsa/MLDsaPublicKey.cs
@@ -11,49 +11,47 @@
 
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Crypto.Signers;
-using System;
 
-namespace Neo.Cryptography
+namespace Neo.Cryptography;
+
+public class MLDsaPublicKey
 {
-    public class MLDsaPublicKey
+    private readonly MLDsaPublicKeyParameters _publicKey;
+
+    internal MLDsaPublicKey(MLDsaPublicKeyParameters publicKey)
     {
-        private readonly MLDsaPublicKeyParameters _publicKey;
+        _publicKey = publicKey;
+    }
 
-        internal MLDsaPublicKey(MLDsaPublicKeyParameters publicKey)
-        {
-            _publicKey = publicKey;
-        }
+    /// <summary>
+    /// Exports the public key of the MLDsa65 with the FIPS 204 public key format.
+    /// </summary>
+    /// <returns>The public key in the FIPS 204 public key format.</returns>
+    public byte[] ExportPublicKey() => _publicKey.GetEncoded();
 
-        /// <summary>
-        /// Exports the public key of the MLDsa65 with the FIPS 204 public key format.
-        /// </summary>
-        /// <returns>The public key in the FIPS 204 public key format.</returns>
-        public byte[] ExportPublicKey() => _publicKey.GetEncoded();
+    /// <summary>
+    /// Imports the public key of the MLDsa65 with the FIPS 204 key format.
+    /// </summary>
+    /// <param name="publicKey">The public key in the FIPS 204 key format.</param>
+    /// <returns>The MLDsa65 public key imported from the public key.</returns>
+    /// <exception cref="ArgumentException">The public key is invalid.</exception>
+    public static MLDsaPublicKey ImportMLDsa65PublicKey(byte[] publicKey)
+    {
+        var key = MLDsaPublicKeyParameters.FromEncoding(MLDsaParameters.ml_dsa_65, publicKey);
+        return new MLDsaPublicKey(key);
+    }
 
-        /// <summary>
-        /// Imports the public key of the MLDsa65 with the FIPS 204 key format.
-        /// </summary>
-        /// <param name="publicKey">The public key in the FIPS 204 key format.</param>
-        /// <returns>The MLDsa65 public key imported from the public key.</returns>
-        /// <exception cref="ArgumentException">The public key is invalid.</exception>
-        public static MLDsaPublicKey ImportMLDsa65PublicKey(byte[] publicKey)
-        {
-            var key = MLDsaPublicKeyParameters.FromEncoding(MLDsaParameters.ml_dsa_65, publicKey);
-            return new MLDsaPublicKey(key);
-        }
-
-        /// <summary>
-        /// Verifies the specified message with the signature of the MLDsa65.
-        /// </summary>
-        /// <param name="message">The message to verify.</param>
-        /// <param name="signature">The signature to verify.</param>
-        /// <returns>True if the message is verified, false otherwise.</returns>
-        public bool Verify(ReadOnlySpan<byte> message, byte[] signature)
-        {
-            var verifier = new MLDsaSigner(_publicKey.Parameters, deterministic: true);
-            verifier.Init(forSigning: false, _publicKey);
-            verifier.BlockUpdate(message);
-            return verifier.VerifySignature(signature);
-        }
+    /// <summary>
+    /// Verifies the specified message with the signature of the MLDsa65.
+    /// </summary>
+    /// <param name="message">The message to verify.</param>
+    /// <param name="signature">The signature to verify.</param>
+    /// <returns>True if the message is verified, false otherwise.</returns>
+    public bool Verify(ReadOnlySpan<byte> message, byte[] signature)
+    {
+        var verifier = new MLDsaSigner(_publicKey.Parameters, deterministic: true);
+        verifier.Init(forSigning: false, _publicKey);
+        verifier.BlockUpdate(message);
+        return verifier.VerifySignature(signature);
     }
 }
